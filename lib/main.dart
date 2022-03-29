03559544a3024185c80a72cc83e0bdd224e6f360
Hanri <henry018@laposte.net>
2022-03-29 16:49:43 +0200

    tested recursive widgets

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,28 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    final Map<String, dynamic> map = {
+      "config" : {
+        "haha" : "hoho",
+        "ihhou" : {
+          "hihi" : "oh no",
+          "hehe" : "yes",
+          "yiha" : {
+            "test1" : "test1",
+            "test2" : "test2"
+          }
+        }
+      }
+    };
+
+    return Scaffold(
+      body: Center(
+        child: TestingWidget(
+          map: map,
+        ),
+      ),
+    );
     return Scaffold(
       body: Center(
         child: ListTile(
@@ -43,6 +65,58 @@ class MyHomePage extends StatelessWidget {
           ),
         ),
       ),
+    );
+  }
+}
+
+class TestingWidget extends StatelessWidget {
+  final Map<String, dynamic> map;
+  const TestingWidget({
+    Key? key,
+    required this.map
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> items = [];
+    map.forEach((key, value) {
+      if (value is String) {
+        items.add(
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [Expanded(child: Text(key)), Spacer(), Expanded(child: Text(value))],
+          )
+        );
+      } else {
+        items.add(
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  Text(key),
+                  Expanded(
+                    child: Divider(
+                      color: Colors.black,
+                    ),
+                  )
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 15),
+                child: TestingWidget(
+                  map: value
+                ),
+              ),
+            ],
+          )
+        );
+      }
+    });
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [...items],
     );
   }
 }
